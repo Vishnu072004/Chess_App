cf9c7995b283bf8a7077905fbe12a0aec10da257
Vishnu072004 <146358001+Vishnu072004@users.noreply.github.com>
2026-02-06 14:57:27 +0530

    bug fix

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import express from "express";
 import cors from "cors";
 import { PORT } from "./config/env.js";
@@ -36,33 +35,4 @@ app.use("/streak", streakRoutes);
 // Start server
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
-=======
-const express = require("express");
-const cors = require("cors");
-
-const app = express();
-app.use(cors({
-    origin: true,
-    credentials: true
-}));
-app.use(express.json());
-
-const dotenv = require("dotenv");
-dotenv.config();
-
-const mongoose = require("mongoose");
-mongoose.connect(process.env.MONGO_URI)
-    .then(() => console.log("MongoDB connected"))
-    .catch(err => console.error(err));
-
-app.use("/auth", require("./routes/loginRoute"));
-app.use("/data", require("./routes/dataRoute"));
-app.use("/admin", require("./routes/adminRoute"));
-app.use("/reels", require("./routes/reelRoutes"));
-app.use("/upload", require("./routes/uploadRoute"));
-app.use("/uploads", express.static("uploads"));
-
-app.listen(process.env.PORT, () => {
-    console.log("Server running on http://localhost:" + process.env.PORT);
->>>>>>> 1cff64e50888257e26bc72353e55aa900e4f0757
 });
